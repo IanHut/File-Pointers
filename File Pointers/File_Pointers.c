9092bfbdcd0e282bd9e189af62e50c09ec454c23
Ian Hutcheson <penelope15@gmail.com>
2018-08-15 18:45:04 -0700

    Add title comments

--- File Pointers/File_Pointers.c
+++ File Pointers/File_Pointers.c
@@ -1,3 +1,15 @@
+// Filename: File_Pointers.c
+//
+// These are notes and sandbox code for the CS50 2018 Week 3 short video
+// entitled "File Pointers".
+//
+// Date: 08/15/2018
+//
+// Name: Ian Hutcheson
+//
+// IDE: Microsoft Visual Studio 2017
+
+
 #include <stdio.h>
 
 void main() {
